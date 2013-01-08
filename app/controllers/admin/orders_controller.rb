04003563f5e96f87f56ee8d17b06305a83027428
Christian Knappskog <ch_knapp@mac.com>
2013-01-08 14:22:59 +0100

    prettified the PDF-generater, and added more information to each order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,14 +8,17 @@ class Admin::OrdersController < ApplicationController
   def show
     @orders = Order.to_be_delivered_on(Date.parse(params[:id]))
 
+    @total = 0
     @meal_types = MealType.all.map do |m|
-      [m.title, m.amount_ordered_for_day(Date.parse(params[:id]))]
+      tot = m.amount_ordered_for_day(Date.parse(params[:id]))
+      @total += tot
+      [m.title, tot]
     end
 
     respond_to do |format|
       format.html # show.html.erb
       format.pdf do 
-        pdf = OrderPdf.new(@orders, @meal_types, params[:id])
+        pdf = OrderPdf.new(@orders, @meal_types, params[:id], @total)
         send_data pdf.render, filename:"orders",type: "application/pdf",
           disposition: "inline"
       end
--- app/pdfs/order_pdf.rb
+++ app/pdfs/order_pdf.rb
@@ -1,31 +1,66 @@
 class OrderPdf < Prawn::Document
-  def initialize(orders, meal_types, date)
-    super(top_margin: 70)
+  def initialize(orders, meal_types, date, total)
+    super(margin: 70, page_size: "A4")
     @orders = orders
     @meal_types = meal_types
-    @date = date
+    @date = Date.parse(date).strftime("%a, %d. %b")
+    @total = total
     generate_orders
   end
 
   def generate_orders
-
-
-    text "Overview for  ##{@date}", :size => 30, :style => :bold
+    text "Oversikt for #{@date}", :size => 30, :style => :bold
 
     move_down(30)
 
-    overview =  [["Meal", "Qty"]] + @meal_types
-    table overview
+    text "#{@total} enheter fordelt paa #{@orders.count} bestillinger", :size=>16, :style => :bold
 
-    start_new_page
+    move_down(30)
+
+
+    overview =  [["Maaltid", "Antall"]] + @meal_types
+    table(overview, :column_widths => {0 => 400, 1=> 50})
+
+    page_number = 0
 
     @orders.each do |order|
+      start_new_page
+      page_number+=1
 
-      text "Order #{order.deliver_to}", :size => 30, :style => :bold
+      details = [["Leveres til:", "Leveringstidspunkt"], [order.deliver_to, order.delivered_at]]
 
-      move_down(30)
+      text_box "#{order.total_order_amount} enheter", :size => 18, :style => :bold, align: :left
+      text "Bestilling ##{order.id}", :size => 16, :style => :italic, align: :right
 
-      items = [["Meal", "Qty"]] +
+      move_down(40)
+
+      text "Leveres til:", :style => :italic
+      text order.user.given_name + " " + order.user.family_name, :size=>18, :style => :bold
+      text order.deliver_to, :size=>18, :style => :bold
+      text "Tlf: #{order.user.phone}", :size=>18, :style => :italic
+
+      text_box "Leveringstidspunkt:", 
+        :at => [350,645],
+        :height => 200,
+        :width => 200,
+        :style => :italic
+
+      text_box order.delivered_at.strftime("%a, %d. %b"),
+        :at => [350,630],
+        :size=>18, :style => :bold,
+        :height => 200,
+        :width => 200
+
+      text_box order.delivered_at.strftime("%H:%M"),
+        :at => [350,600],
+        :size=>30, :style => :bold,
+        :height => 200,
+        :width => 200
+
+    horizontal_line 0, 450, :at => 500
+      move_down(100)
+
+      items = [["Maaltid", "Antall"]] +
         order.meals.map do |item|
         [
           item.meal_type.title,
@@ -33,15 +68,14 @@ class OrderPdf < Prawn::Document
         ]
         end
 
-      table items
+      table(items, 
+          :column_widths => {0 => 400, 1=> 50})
 
-      move_down(10)
+      move_down(20)
 
-      text "Total #{order.total_order_amount}", :size => 16, :style => :bold
+      text "Total #{order.total_order_amount}", :size => 16, :style => :bold, :align => :right
 
-      start_new_page
-
+      text_box page_number.to_s + "/" + @orders.count.to_s, :at => [430, 20], :size => 16
     end
   end
-
 end
