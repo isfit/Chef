906d482f3f6212c031fb1f447db52683979d051e
Christian Knappskog <ch_knapp@mac.com>
2013-01-30 18:15:33 +0100

    trying to fix error

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -105,7 +105,7 @@ class OrdersController < ApplicationController
         format.html { redirect_to @order, notice: 'Order was successfully created.' }
         format.json { render json: @order, status: :created, location: @order }
       else
-        format.html { render action: "new" }
+        format.html { redirect_to action: "new" }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
@@ -121,7 +121,7 @@ class OrdersController < ApplicationController
         format.html { redirect_to @order, notice: 'Order was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
+        format.html { redirect_to action: "edit" }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
