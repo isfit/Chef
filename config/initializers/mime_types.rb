0fd94df630d51f481e768676e8145741c75487d3
Kristian Våge <krisvaage@gmail.com>
2013-09-05 12:48:40 +0200

    Removed pdf initialization, done by default in rails 3.2

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,7 +3,7 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
-Mime::Type.register "application/pdf", :pdf
+# Mime::Type.register "application/pdf", :pdf
 
 # https://groups.google.com/forum/#!topic/rubyonrails-security/61bkgvnSGTQ/discussion
 ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
