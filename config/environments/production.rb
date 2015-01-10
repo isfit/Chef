99c8bc94c583082a6ba463ea7273d081684384df
Thomas Gjerde <thomasgjerde90@gmail.com>
2015-01-10 15:06:47 +0100

    Slightly fixier fix than the last one

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,14 @@ Chef::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   :location => '/usr/sbin/sendmail',
+  #   :arguments => '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.middleware.use ExceptionNotification::Rack,
                         :ignore_exceptions => ['CanCan::AccessDenied'] + ExceptionNotifier.ignored_exceptions,
                         :ignore_crawlers => %w{Googlebot bingbot},
